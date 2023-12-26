dc3fbe08813b19998f948ea1bd9bae8d06654822
IMartinezA <33355502+IMartinezA@users.noreply.github.com>
2023-12-26 13:10:12 -0300

    Laplace examples update

---
+++ biosspheres/laplace/examples/plottings.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+from biosspheres.laplace.selfinteractions import v_jj_azimuthal_symmetry
+
+
+def observing_v(
+        big_l: int = 50,
+        r: float = 2.5
+) -> None:
+    """
+    Plots the V operator. It shows the plot.
+
+    Parameters
+    ----------
+    big_l : int
+        >= 0, max degree.
+    r : float
+        > 0, radius.
+
+    Returns
+    -------
+    None
+
+    See Also
+    --------
+    biosspheres.laplace.selfinteractions.v_jj_azimuthal_symmetry
+    
+    """
+    v = v_jj_azimuthal_symmetry(big_l, r)
+    plt.figure()
+    plt.plot(
+        v,
+        marker='x'
+    )
+    plt.title('V')
+    plt.xlabel('l')
+    plt.show()
+    pass
+
+
+if __name__ == '__main__':
+    observing_v()
