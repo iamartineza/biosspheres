8328fef281535e492879e8006c35fee1f0f57ee2
IMartinezA <33355502+IMartinezA@users.noreply.github.com>
2023-12-26 15:05:15 -0300

    adding of azimuthal_trace_to_general_with_zeros_linear_operator

--- biosspheres/miscella/extensions.py
+++ biosspheres/miscella/extensions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.sparse.linalg
 
 
 def azimuthal_trace_to_general_with_zeros(
@@ -27,3 +28,21 @@ def azimuthal_trace_to_general_with_zeros(
     general_trace = np.zeros(num ** 2, dtype=trace.dtype)
     general_trace[eles_times_2_plus_one] = trace[eles]
     return general_trace
+
+
+def azimuthal_trace_to_general_with_zeros_linear_operator(
+        big_l: int
+) -> scipy.sparse.linalg.LinearOperator:
+    num = big_l + 1
+    eles = np.arange(0, num)
+    eles_times_2_plus_one = eles * (eles + 1)
+
+    def general_trace(trace) -> np.ndarray:
+        x = np.zeros(num**2)
+        x[eles_times_2_plus_one] = trace[eles]
+        return x
+
+    general_trace_operator = scipy.sparse.linalg.LinearOperator(
+        (num**2, num), matvec=general_trace())
+
+    return general_trace_operator
