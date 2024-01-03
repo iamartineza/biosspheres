f30d12648cfdc055c963597108c7e585c05285c4
IMartinezA <33355502+IMartinezA@users.noreply.github.com>
2024-01-03 18:29:55 -0300

    added test for the solution of n spheres with the reduced version, checking is the same as using the not reduced one

--- biosspheres/formulations/mtf/tests/consistencelaplace.py
+++ biosspheres/formulations/mtf/tests/consistencelaplace.py
@@ -14,7 +14,7 @@ import biosspheres.laplace.crossinteractions as laplacecross
 import biosspheres.miscella.extensions as extensions
 
 
-def testing_mtf_linear_operators_and_matrices_laplace_one_sphere(
+def testing_mtf_linear_operators_and_matrices_one_sphere(
         big_l: int = 5, r: float = 2.1, pi: float = 3.
 ) -> None:
     """
@@ -93,7 +93,7 @@ def testing_mtf_linear_operators_and_matrices_laplace_one_sphere(
     pass
 
 
-def testing_mtf_linear_operators_and_matrices_laplace_n_spheres(
+def testing_mtf_linear_operators_and_matrices_n_spheres(
 ) -> None:
     n = 3
     big_l = 5
@@ -141,7 +141,7 @@ def testing_mtf_linear_operators_and_matrices_laplace_n_spheres(
     
     solution_indirect, info = scipy.sparse.linalg.gmres(
         linear_operator, b,
-        tol=10**(-10),
+        tol=tolerance,
         restart=(4 * num)**3,
         callback=callback_function,
         callback_type='pr_norm')
@@ -153,7 +153,7 @@ def testing_mtf_linear_operators_and_matrices_laplace_n_spheres(
     pass
 
 
-def testing_mtf_azimuthal_and_no_azimuthal_laplace(
+def testing_mtf_azimuthal_and_no_azimuthal(
         big_l: int = 5, r: float = 2.1, pi: float = 3.
 ) -> None:
     """
@@ -247,7 +247,7 @@ def testing_mtf_azimuthal_and_no_azimuthal_laplace(
     pass
 
 
-def testing_mtf_reduced_linear_operators_and_matrices_laplace(
+def testing_mtf_reduced_linear_operators_and_matrices(
         big_l: int = 5, r: float = 2.1, pi: float = 3.
 ) -> None:
     """
@@ -317,7 +317,7 @@ def testing_mtf_reduced_linear_operators_and_matrices_laplace(
     pass
 
 
-def testing_mtf_reduced_azimuthal_and_no_azimuthal_laplace(
+def testing_mtf_reduced_azimuthal_and_no_azimuthal(
         big_l: int = 5, r: float = 2.1, pi: float = 3.
 ) -> None:
     """
@@ -392,7 +392,7 @@ def testing_mtf_reduced_azimuthal_and_no_azimuthal_laplace(
     pass
 
 
-def testing_mtf_reduced_vs_not_laplace(
+def testing_mtf_reduced_vs_not_one_sphere(
         big_l: int = 5, r: float = 2.1, pi: float = 3.
 ) -> None:
     """
@@ -460,16 +460,66 @@ def testing_mtf_reduced_vs_not_laplace(
     sol_red_2[:] = sol_red_2[:] / (r**4)
     sol_red_2[:] = 2. * a_1_lin_op.matvec(sol_red_2[:])
     solution_red_1 = np.concatenate((sol_red_1, sol_red_2))
-    print('\nRunning function testing_mtf_reduced_vs_not')
+    print('\nRunning function testing_mtf_reduced_vs_not_one_sphere')
     print('- The following should be zero or near.')
     print(np.linalg.norm(solution_red_1 - solution))
     pass
 
 
+def testing_mtf_reduced_vs_not_n_spheres() -> None:
+    n = 3
+    big_l = 5
+    big_l_c = 10
+    radii = np.ones(n) * 1.112
+    center_positions = [np.asarray([0., 0., 0]), np.asarray([-7., -3., -2.]),
+                        np.asarray([3., 5., 7.])]
+    sigmas = np.ones(n + 1) * 0.75
+    sigmas[0] = 3.
+    sigmas[1] = 0.4
+    sigmas[2] = 1.7
+    p0 = np.asarray([5., -5., 5.])
+    
+    num = (big_l + 1)**2
+    
+    pii = sigmas[1:len(sigmas)] / sigmas[0]
+    mass_n_two = mass.n_two_j_blocks(big_l, radii, azimuthal=False)
+    x_dia, x_dia_inv = mtf.x_diagonal_with_its_inv(
+        n, big_l, radii, pii, azimuthal=False)
+    big_a_0_cross = laplacecross.all_cross_interactions_n_spheres_v2d(
+        n, big_l, big_l_c, radii, center_positions)
+    sparse_big_a_0_self, sparse_big_a_n = laplaceself.a_0_a_n_sparse_matrices(
+        n, big_l, radii, azimuthal=False)
+    
+    b = righthands.b_vector_n_spheres_mtf_point_source(n, big_l,
+                                                       center_positions, p0,
+                                                       radii, sigmas[0], x_dia,
+                                                       mass_n_two)
+    # Not reduced system
+    matrix = mtf.mtf_n_matrix(
+        big_a_0_cross, sparse_big_a_0_self, sparse_big_a_n, x_dia, x_dia_inv)
+    
+    solution_not_reduced = np.linalg.solve(matrix, b)
+    
+    # Reduced system
+    sparse_reduced = laplaceself.reduced_a_sparse_matrix(
+        n, big_l, radii, pii, azimuthal=False)
+    matrix = mtf.mtf_n_reduced_matrix(big_a_0_cross, sparse_reduced)
+    b_red_1 = b[0:2*n*num] + 2. * sparse_big_a_n.dot(b[2 * n*num:4 * n*num]) / x_dia
+    sol_red_1 = np.linalg.solve(matrix, b_red_1)
+    sol_red_2 = (b[2 * n*num:4 * n*num] + x_dia * sol_red_1) / mass_n_two**2
+    sol_red_2[:] = 2. * sparse_big_a_n.dot(sol_red_2[:])
+    solution_reduced = np.concatenate((sol_red_1, sol_red_2))
+    print('\nRunning function testing_mtf_reduced_vs_not_n_spheres')
+    print('- The following should be zero or near.')
+    print(np.linalg.norm(solution_not_reduced - solution_reduced))
+    pass
+
+
 if __name__ == '__main__':
-    testing_mtf_linear_operators_and_matrices_laplace_n_spheres()
-    testing_mtf_linear_operators_and_matrices_laplace_one_sphere()
-    testing_mtf_azimuthal_and_no_azimuthal_laplace()
-    testing_mtf_reduced_linear_operators_and_matrices_laplace()
-    testing_mtf_reduced_azimuthal_and_no_azimuthal_laplace()
-    testing_mtf_reduced_vs_not_laplace()
+    testing_mtf_reduced_vs_not_n_spheres()
+    testing_mtf_linear_operators_and_matrices_one_sphere()
+    testing_mtf_linear_operators_and_matrices_n_spheres()
+    testing_mtf_azimuthal_and_no_azimuthal()
+    testing_mtf_reduced_linear_operators_and_matrices()
+    testing_mtf_reduced_azimuthal_and_no_azimuthal()
+    testing_mtf_reduced_vs_not_one_sphere()
