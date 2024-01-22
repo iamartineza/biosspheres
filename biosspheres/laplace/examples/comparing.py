5adad67d55bfbf3c11f72dae2885552dab5e812a
IMartinezA <33355502+IMartinezA@users.noreply.github.com>
2024-01-22 18:01:57 -0300

    small changes

--- biosspheres/laplace/examples/comparing.py
+++ biosspheres/laplace/examples/comparing.py
@@ -27,10 +27,8 @@ def v_1d_vs_2d() -> None:
     
     quantity_theta_points, quantity_phi_points, weights, pre_vector_t_2d = \
         quadratures.gauss_legendre_trapezoidal_2d(big_l_c)
-    (r_coord_1tf_2d, phi_coord_1tf_2d, cos_theta_coord_1tf_2d,
-     er_times_n_1tf_2d, etheta_times_n_1tf_2d, ephi_times_n_1tf_2d) = \
-        (quadratures.
-        from_sphere_s_cartesian_to_j_spherical_and_spherical_vectors_2d(
+    r_coord_1tf_2d, phi_coord_1tf_2d, cos_theta_coord_1tf_2d = \
+        (quadratures.from_sphere_s_cartesian_to_j_spherical_2d(
             radio_2, p_1, p_2, quantity_theta_points, quantity_phi_points,
             pre_vector_t_2d))
     pesykus, p2_plus_p_plus_q, p2_plus_p_minus_q = auxindexes.pes_y_kus(big_l)
