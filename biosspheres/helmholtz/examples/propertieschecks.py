fe7d8bfe12510702b86ec75f82f08a2ac5756ac5
IMartinezA <33355502+IMartinezA@users.noreply.github.com>
2024-01-23 11:59:48 -0300

    added function for checking k_0_sj_semi_analytic_v1d

--- biosspheres/helmholtz/examples/propertieschecks.py
+++ biosspheres/helmholtz/examples/propertieschecks.py
@@ -61,6 +61,52 @@ def v_transpose_check() -> None:
     pass
 
 
+def k_with_v_check() -> None:
+    radio_1 = 1.2
+    radio_2 = 3.
+    
+    p_1 = np.asarray([2., 3., 4.])
+    p_2 = -p_1
+    
+    k0 = 7.
+    
+    big_l = 2
+    big_l_c = 25
+    
+    eles = np.arange(0, big_l + 1)
+    j_l_1 = scipy.special.spherical_jn(eles, radio_1 * k0)
+    j_lp_1 = scipy.special.spherical_jn(eles, radio_1 * k0)
+    
+    final_length, pre_vector_t, transform = \
+        quadratures.complex_spherical_harmonic_transform_1d(big_l, big_l_c)
+    r_coord_1tf, phi_coord_1tf, cos_theta_coord_1tf = (
+        quadratures.
+        from_sphere_s_cartesian_to_j_spherical_1d(
+            radio_2, p_1, p_2, final_length, pre_vector_t))
+    
+    data_v21 = crossinteractions.v_0_sj_semi_analytic_v1d(
+        big_l, k0, radio_1, radio_2, j_l_1, r_coord_1tf, phi_coord_1tf,
+        cos_theta_coord_1tf, final_length, transform)
+    
+    data_k21 = crossinteractions.k_0_sj_semi_analytic_v1d(
+        big_l, k0, radio_1, radio_2, j_lp_1, r_coord_1tf, phi_coord_1tf,
+        cos_theta_coord_1tf, final_length, transform)
+    
+    jeys_array = np.diag(np.repeat(-k0 * (j_lp_1 / j_l_1), 2 * eles + 1))
+    
+    aux = data_v21@jeys_array
+    
+    plt.figure(dpi=75., layout='constrained')
+    im = np.abs(data_k21 - aux) / np.abs(data_k21)
+    plt.imshow(im, cmap='RdBu')
+    plt.colorbar()
+    plt.title('Relative error between $K_{2,1}^0$ and'
+              ' a transformation of $V_{2,1}^{0}$.')
+    plt.show()
+    pass
+
+
 if __name__ == '__main__':
     v_transpose_check()
+    k_with_v_check()
     
