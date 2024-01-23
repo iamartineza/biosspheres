d4cf9361e2e5d2eb5fd3e1acde220ed728c65582
IMartinezA <33355502+IMartinezA@users.noreply.github.com>
2024-01-23 11:59:31 -0300

    added function k_0_sj_semi_analytic_v1d

--- biosspheres/helmholtz/crossinteractions.py
+++ biosspheres/helmholtz/crossinteractions.py
@@ -142,7 +142,7 @@ def v_0_sj_semi_analytic_v1d(
                    axis=1, out=data_v[:, l_square_plus_l[el] - m])
             data_v[:, l_square_plus_l[el] - m] = (
                     j_l_j[el] * data_v[:, l_square_plus_l[el] - m])
-    data_v[:] = 1j * k0 * r_j**2 * r_s**2 * data_v[:]
+    data_v[:] = 1j * k0 * (r_j * r_s)**2 * data_v[:]
     return data_v
 
 
@@ -404,3 +404,145 @@ def v_0_js_from_v_0_sj(
                       l_square_plus_l[el] - el:l_square_plus_l[el] + el + 1]))
     data_v_js = giro_array @ sign_array @ data_v_sj.T @ sign_array @ giro_array
     return data_v_js
+
+
+def k_0_sj_semi_analytic_v1d(
+        big_l: int,
+        k0: float,
+        r_j: float,
+        r_s: float,
+        j_lp_j: np.ndarray,
+        r_coord: np.ndarray,
+        phi_coord: np.ndarray, cos_theta_coord: np.ndarray,
+        final_length: int,
+        transform: np.ndarray
+) -> np.ndarray:
+    """
+    Returns a numpy array that represents a numerical approximation of
+    the matrix formed by the boundary integral operator K_{s,j}^0 with
+    Laplace kernel evaluated and tested with spherical harmonics.
+    In this routine the quadrature points NEED to be ordered in an array
+    of one dimension.
+    It is a SLOW routine, because it does not use any symmetry or
+    properties of the spherical harmonics.
+
+    Notes
+    -----
+    data_k[p*(2p+1) + q, l*(2l+1) + m] =
+        ( K_{s,j}^0 Y_{l,m,j} ; Y_{p,q,s} )_{L^2(S_s)}.
+    Y_{l,m,j} : spherical harmonic degree l, order m, in the coordinate
+        system j.
+    S_s : surface of the sphere s.
+
+    The expression K_{s,j}^0 Y_l,m,j can be obtained analytically. A
+    quadrature scheme is used to compute the surface integral
+    corresponding to the inner product.
+
+    In this routine the quadrature points NEED to be ordered in an array
+    of one dimension.
+    It is a SLOW routine, because it does not use any symmetry or
+    properties of the spherical harmonics.
+
+    Parameters
+    ----------
+    big_l : int
+        >= 0, max degree.
+    k0 : float
+        > 0
+    r_j : float
+        > 0, radius of the sphere j.
+    r_s : float
+        > 0, radius of the sphere s.
+    j_lp_j : np.ndarray
+        of floats.
+    r_coord : np.ndarray
+        Array of floats with the spherical coordinate r of the
+        quadrature points in the coordinate system s. Length equals to
+        final_length. Can come from the function
+        from_sphere_s_cartesian_to_j_spherical_1d of the module
+        biosspheres.quadratures.spheres.
+    phi_coord : np.ndarray
+        Array of floats with the phi coordinate r of the quadrature
+        points in the coordinate system s. Length equals to
+        final_length. Can come from the function
+        from_sphere_s_cartesian_to_j_spherical_1d of the module
+        biosspheres.quadratures.spheres.
+    cos_theta_coord : np.ndarray
+        Array of floats with the coseno of the spherical coordinate
+        theta of the quadrature points in the coordinate system s.
+        Lengths equal to final_length. Can come from the function
+        from_sphere_s_cartesian_to_j_spherical_1d of the module
+        biosspheres.quadratures.spheres.
+    final_length : int
+        How many points for the surface integral.
+        Can come from the function spherical_harmonic_transform_1d of
+        the module biosspheres.quadratures.spheres.
+    transform : np.ndarray
+        of floats with the evaluation of the spherical harmonics along
+        with their weights in the quadrature points.
+        Can come from the function real_spherical_harmonic_transform_1d
+        of the module biosspheres.quadratures.spheres.
+
+    Returns
+    -------
+    data_k : numpy array
+        Shape ((big_l+1)**2, (big_l+1)**2). See notes for the indexes ordering.
+
+    See Also
+    --------
+    k_0_sj_semi_analytic_v2d
+    k_0_sj_from_v_0_sj
+
+    """
+    argument = k0 * r_coord
+    
+    num = big_l + 1
+    eles = np.arange(0, num)
+    
+    legendre_functions = \
+        np.empty((num * (big_l + 2) // 2, final_length))
+    h_l = np.empty((final_length, big_l + 1), dtype=np.complex128)
+    for i in np.arange(0, final_length):
+        h_l[i, :] = (scipy.special.spherical_jn(eles, argument[i])
+                     + 1j * scipy.special.spherical_yn(eles, argument[i]))
+        legendre_functions[:, i] = \
+            pyshtools.legendre.PlmON(big_l, cos_theta_coord[i],
+                                     csphase=-1, cnorm=1)
+    
+    exp_pos = np.empty((big_l, final_length), dtype=np.complex128)
+    for m in np.arange(1, big_l + 1):
+        np.exp(1j * m * phi_coord, out=exp_pos[m - 1, :])
+    
+    el_plus_1_square = num**2
+    data_k = np.empty((el_plus_1_square, el_plus_1_square),
+                      dtype=np.complex128)
+    
+    eles = np.arange(0, num)
+    eles_plus_1 = eles + 1
+    
+    l_square_plus_l = eles_plus_1 * eles
+    l_times_l_plus_l_divided_by_2 = l_square_plus_l // 2
+    
+    temp_l = np.zeros_like(transform)
+    temp_l_m = np.zeros_like(temp_l)
+    for el in eles:
+        temp_l[:] = h_l[:, el] * transform
+        np.sum(
+            temp_l * legendre_functions[l_times_l_plus_l_divided_by_2[el], :],
+            axis=1, out=data_k[:, l_square_plus_l[el]])
+        data_k[:, l_square_plus_l[el]] = (
+                j_lp_j[el] * data_k[:, l_square_plus_l[el]])
+        for m in np.arange(1, el + 1):
+            temp_l_m[:] = (temp_l *
+                           legendre_functions[l_times_l_plus_l_divided_by_2[el]
+                                              + m, :])
+            np.sum(temp_l_m * exp_pos[m - 1, :], axis=1,
+                   out=data_k[:, l_square_plus_l[el] + m])
+            data_k[:, l_square_plus_l[el] + m] = (
+                    j_lp_j[el] * data_k[:, l_square_plus_l[el] + m])
+            np.sum(temp_l_m * (-1)**m / exp_pos[m - 1, :], axis=1,
+                   out=data_k[:, l_square_plus_l[el] - m])
+            data_k[:, l_square_plus_l[el] - m] = (
+                    j_lp_j[el] * data_k[:, l_square_plus_l[el] - m])
+    data_k[:] = -1j * (k0 * r_j * r_s)**2 * data_k[:]
+    return data_k
