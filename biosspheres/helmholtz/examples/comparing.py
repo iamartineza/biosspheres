6497b68488b2d33049db706ebae4c7c756b4a48e
IMartinezA <33355502+IMartinezA@users.noreply.github.com>
2024-01-29 19:53:27 -0300

    added checkings of the for the W operator

--- biosspheres/helmholtz/examples/comparing.py
+++ biosspheres/helmholtz/examples/comparing.py
@@ -296,6 +296,104 @@ def ka_js_from_the_other() -> None:
     pass
 
 
+def w_1d_vs_2d() -> None:
+    radio_1 = 3.
+    radio_2 = 2.
+    
+    p_1 = np.asarray([2., 3., 4.])
+    p_2 = -p_1
+    
+    k0 = 7.
+    
+    big_l = 5
+    big_l_c = 25
+    
+    j_lp_1 = scipy.special.spherical_jn(np.arange(0, big_l + 1), radio_1 * k0,
+                                        derivative=True)
+    
+    final_length, pre_vector_t, transform = \
+        quadratures.complex_spherical_harmonic_transform_1d(big_l, big_l_c)
+    (r_coord_1tf, phi_coord_1tf, cos_theta_coord_1tf,
+     er_1tf, eth_1tf, ephi_1tf) = (
+        quadratures.
+        from_sphere_s_cartesian_to_j_spherical_and_spherical_vectors_1d(
+            radio_2, p_1, p_2, final_length, pre_vector_t))
+    data_ka21 = crossinteractions.w_0_sj_semi_analytic_recurrence_v1d(
+        big_l, k0, radio_1, radio_2, j_lp_1, r_coord_1tf, phi_coord_1tf,
+        cos_theta_coord_1tf, er_1tf, eth_1tf, ephi_1tf, final_length,
+        transform)
+    
+    quantity_theta_points, quantity_phi_points, weights, pre_vector_t_2d = \
+        quadratures.gauss_legendre_trapezoidal_2d(big_l_c)
+    (r_coord_1tf_2d, phi_coord_1tf_2d, cos_theta_coord_1tf_2d,
+     er_1tf_2d, eth_1tf_2d, ephi_1tf_2d) = (
+        quadratures.
+        from_sphere_s_cartesian_to_j_spherical_and_spherical_vectors_2d(
+            radio_2, p_1, p_2, quantity_theta_points, quantity_phi_points,
+            pre_vector_t_2d))
+    pesykus, p2_plus_p_plus_q, p2_plus_p_minus_q = auxindexes.pes_y_kus(big_l)
+    data_ka21_2d = crossinteractions.w_0_sj_semi_analytic_recurrence_v2d(
+        big_l, k0, radio_1, radio_2, j_lp_1, r_coord_1tf_2d, phi_coord_1tf_2d,
+        cos_theta_coord_1tf_2d, er_1tf_2d, eth_1tf_2d, ephi_1tf_2d, weights,
+        pre_vector_t_2d[2, :, 0], quantity_theta_points, quantity_phi_points,
+        pesykus, p2_plus_p_plus_q, p2_plus_p_minus_q)
+    
+    plt.figure(dpi=75., layout='constrained')
+    im = np.abs(data_ka21_2d - data_ka21)
+    plt.imshow(im, cmap='RdBu')
+    plt.colorbar()
+    plt.title('Absolute error between $W_{2,1}^{0}$ with 1d and 2d routines')
+    plt.show()
+    pass
+
+
+def w_js_from_the_other() -> None:
+    radio_1 = 3.
+    radio_2 = 2.
+    
+    p_1 = np.asarray([2., 3., 4.])
+    p_2 = -p_1
+    
+    k0 = 7.
+    
+    big_l = 3
+    big_l_c = 25
+    
+    j_l_1 = scipy.special.spherical_jn(np.arange(0, big_l + 1), radio_1 * k0)
+    j_lp_1 = scipy.special.spherical_jn(np.arange(0, big_l + 1), radio_1 * k0,
+                                        derivative=True)
+    
+    quantity_theta_points, quantity_phi_points, weights, pre_vector_t_2d = \
+        quadratures.gauss_legendre_trapezoidal_2d(big_l_c)
+    (r_coord_1tf_2d, phi_coord_1tf_2d, cos_theta_coord_1tf_2d,
+     er_1tf_2d, eth_1tf_2d, ephi_1tf_2d) = (
+        quadratures.
+        from_sphere_s_cartesian_to_j_spherical_and_spherical_vectors_2d(
+            radio_2, p_1, p_2, quantity_theta_points, quantity_phi_points,
+            pre_vector_t_2d))
+    pesykus, p2_plus_p_plus_q, p2_plus_p_minus_q = auxindexes.pes_y_kus(big_l)
+    data_ka21_2d = crossinteractions.ka_0_sj_semi_analytic_recurrence_v2d(
+        big_l, k0, radio_1, radio_2, j_l_1, r_coord_1tf_2d, phi_coord_1tf_2d,
+        cos_theta_coord_1tf_2d, er_1tf_2d, eth_1tf_2d, ephi_1tf_2d, weights,
+        pre_vector_t_2d[2, :, 0], quantity_theta_points, quantity_phi_points,
+        pesykus, p2_plus_p_plus_q, p2_plus_p_minus_q)
+    data_w21_2d = crossinteractions.w_0_sj_semi_analytic_recurrence_v2d(
+        big_l, k0, radio_1, radio_2, j_lp_1, r_coord_1tf_2d, phi_coord_1tf_2d,
+        cos_theta_coord_1tf_2d, er_1tf_2d, eth_1tf_2d, ephi_1tf_2d, weights,
+        pre_vector_t_2d[2, :, 0], quantity_theta_points, quantity_phi_points,
+        pesykus, p2_plus_p_plus_q, p2_plus_p_minus_q)
+    
+    data_w21 = crossinteractions.w_0_sj_from_ka_sj(data_ka21_2d, k0, radio_1)
+    plt.figure(dpi=75., layout='constrained')
+    im = np.abs(data_w21_2d - data_w21)
+    plt.imshow(im, cmap='RdBu')
+    plt.colorbar()
+    plt.title('Absolute error between $W_{2,1}^{0}$ with direct and indirect '
+              'routines')
+    plt.show()
+    pass
+
+
 if __name__ == '__main__':
     v_1d_vs_2d()
     v_js_from_the_other()
@@ -303,3 +401,5 @@ if __name__ == '__main__':
     k_0_sj_from_the_other()
     ka_1d_vs_2d()
     ka_js_from_the_other()
+    w_1d_vs_2d()
+    w_js_from_the_other()
