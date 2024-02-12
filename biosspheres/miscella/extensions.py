965aaa74ff03de9aa110969f37e4b11f35914aba
IMartinezA <33355502+IMartinezA@users.noreply.github.com>
2024-02-12 17:31:14 -0300

    Code reformatted using Black

--- biosspheres/miscella/extensions.py
+++ biosspheres/miscella/extensions.py
@@ -2,13 +2,12 @@ import numpy as np
 
 
 def azimuthal_trace_to_general_with_zeros(
-        big_l: int,
-        trace: np.ndarray
+    big_l: int, trace: np.ndarray
 ) -> np.ndarray:
     """
     Returns the extension by 0, for all m, of an array that has entries
     for all m = 0
-    
+
     Parameters
     ----------
     big_l : int
@@ -19,11 +18,11 @@ def azimuthal_trace_to_general_with_zeros(
     Returns
     -------
     general_trace : np.ndarray
-    
+
     """
     num = big_l + 1
     eles = np.arange(0, num)
     eles_times_2_plus_one = eles * (eles + 1)
-    general_trace = np.zeros(num ** 2, dtype=trace.dtype)
+    general_trace = np.zeros(num**2, dtype=trace.dtype)
     general_trace[eles_times_2_plus_one] = trace[eles]
     return general_trace
