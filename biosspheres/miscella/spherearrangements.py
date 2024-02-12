7879f1fcd0aa7d0df1bf8d72cc4276096bb995f2
IMartinezA <33355502+IMartinezA@users.noreply.github.com>
2024-02-12 17:29:21 -0300

    Code reformated using Black

--- biosspheres/miscella/spherearrangements.py
+++ biosspheres/miscella/spherearrangements.py
@@ -1,21 +1,17 @@
 import numpy as np
 
 
-def cube_vertex_positions(
-        number: int,
-        r: float,
-        d: float
-) -> list[np.ndarray]:
+def cube_vertex_positions(number: int, r: float, d: float) -> list[np.ndarray]:
     """
     To obtain an arrangement of number**3 spheres.
-    
+
     Notes
     -----
     This functions assumes the radii of all spheres are the same, and
     orders them in the corners of an array of cubes, starting in the
     origin of the coordinate system and placing them in the first
     quadrant.
-    
+
     Parameters
     ----------
     number: int
@@ -25,26 +21,26 @@ def cube_vertex_positions(
     d: float
         distance between two surfaces of spheres. The length of the edge
         of each cube 2r + d. If d =0 the spheres will touch each other.
-    
+
     Returns
     -------
     ps
         position of the center of the spheres.
-    
+
     """
     ps = []
-    p = np.array([0., 0., 0.])
+    p = np.array([0.0, 0.0, 0.0])
     two_r_plus_d = 2 * r + d
     for ii in np.arange(0, number):
-        p[2] = 0.
-        p = p + np.array([0., 0., two_r_plus_d * ii])
+        p[2] = 0.0
+        p = p + np.array([0.0, 0.0, two_r_plus_d * ii])
         for jj in np.arange(0, number):
-            p[1] = 0.
-            p = p + np.array([0., two_r_plus_d * jj, 0.])
+            p[1] = 0.0
+            p = p + np.array([0.0, two_r_plus_d * jj, 0.0])
             for zz in np.arange(0, number):
-                p[0] = 0.
-                ps.append(p + np.array([two_r_plus_d * zz, 0., 0.]))
-                p = p + np.array([two_r_plus_d * zz, 0., 0.])
+                p[0] = 0.0
+                ps.append(p + np.array([two_r_plus_d * zz, 0.0, 0.0]))
+                p = p + np.array([two_r_plus_d * zz, 0.0, 0.0])
                 pass
             pass
         pass
