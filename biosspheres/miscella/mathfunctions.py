343439e66cf6c572772dae9bb87de7737535e55a
IMartinezA <33355502+IMartinezA@users.noreply.github.com>
2024-02-12 17:30:09 -0300

    Code reformated using Black

--- biosspheres/miscella/mathfunctions.py
+++ biosspheres/miscella/mathfunctions.py
@@ -1,36 +1,24 @@
 import numpy as np
 
 
-def cte(
-        c: float
-) -> float:
+def cte(c: float) -> float:
     return c
 
 
-def linear_function_3d(
-        coefficients: np.ndarray,
-        vector: np.ndarray
-) -> float:
+def linear_function_3d(coefficients: np.ndarray, vector: np.ndarray) -> float:
     return np.dot(coefficients, vector)
 
 
 def plane_wave(
-        a: float,
-        k: np.ndarray,
-        r: np.ndarray,
-        p: np.ndarray
+    a: float, k: np.ndarray, r: np.ndarray, p: np.ndarray
 ) -> complex:
-    return a*np.exp(np.dot(k, (r + p)) * 1j)
+    return a * np.exp(np.dot(k, (r + p)) * 1j)
 
 
-def point_source(
-        r: np.ndarray,
-        p: np.ndarray,
-        sigma_e: float
-) -> float:
+def point_source(r: np.ndarray, p: np.ndarray, sigma_e: float) -> float:
     """
     Returns 1. / (4 * pi * sigma_e * ||r - p||_2)
-    
+
     Notes
     -----
     All arrays that represent vectors are in the cartesian coordinate
@@ -50,18 +38,17 @@ def point_source(
     Returns
     -------
     float
-    
+
     """
-    return 1. / (4 * np.pi * sigma_e * np.linalg.norm(r - p))
+    return 1.0 / (4 * np.pi * sigma_e * np.linalg.norm(r - p))
 
 
 def laplace_almost_fundamental_function(
-        r_1: np.ndarray, r_2: np.ndarray,
-        p_1: np.ndarray, p_2: np.ndarray
+    r_1: np.ndarray, r_2: np.ndarray, p_1: np.ndarray, p_2: np.ndarray
 ) -> float:
     """
     Evaluating in two DIFFERENT points.
-    
+
     Parameters
     ----------
     r_1
@@ -72,7 +59,7 @@ def laplace_almost_fundamental_function(
     Returns
     -------
     value : float
-    
+
     """
-    value = 1. / np.linalg.norm(p_1 + r_1 - (p_2 + r_2))
+    value = 1.0 / np.linalg.norm(p_1 + r_1 - (p_2 + r_2))
     return value
