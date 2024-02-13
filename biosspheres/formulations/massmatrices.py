3da5686306c78d74518a8e6177ef4b6755d04726
IMartinezA <33355502+IMartinezA@users.noreply.github.com>
2024-02-13 11:06:12 -0300

    Code reformatted with Black.

--- biosspheres/formulations/massmatrices.py
+++ biosspheres/formulations/massmatrices.py
@@ -1,11 +1,7 @@
 import numpy as np
 
 
-def j_block(
-        big_l: int,
-        r: float,
-        azimuthal: bool = True
-) -> np.ndarray:
+def j_block(big_l: int, r: float, azimuthal: bool = True) -> np.ndarray:
     num = big_l + 1
     if not azimuthal:
         num = num**2
@@ -13,11 +9,7 @@ def j_block(
     return mass_matrix
 
 
-def two_j_blocks(
-        big_l: int,
-        r: float,
-        azimuthal: bool = True
-) -> np.ndarray:
+def two_j_blocks(big_l: int, r: float, azimuthal: bool = True) -> np.ndarray:
     num = big_l + 1
     if not azimuthal:
         num = num**2
@@ -26,30 +18,28 @@ def two_j_blocks(
 
 
 def n_j_blocks(
-        big_l: int,
-        radii: np.ndarray,
-        azimuthal: bool = True
+    big_l: int, radii: np.ndarray, azimuthal: bool = True
 ) -> np.ndarray:
     num = big_l + 1
     if not azimuthal:
         num = num**2
     mass_matrix = np.ones(len(radii) * num)
     for j in np.arange(0, len(radii)):
-        mass_matrix[j*num:(j+1)*num] = (
-                radii[j]**2 * mass_matrix[j*num:(j+1)*num])
+        mass_matrix[j * num : (j + 1) * num] = (
+            radii[j] ** 2 * mass_matrix[j * num : (j + 1) * num]
+        )
     return mass_matrix
 
 
 def n_two_j_blocks(
-        big_l: int,
-        radii: np.ndarray,
-        azimuthal: bool = True
+    big_l: int, radii: np.ndarray, azimuthal: bool = True
 ) -> np.ndarray:
     num = big_l + 1
     if not azimuthal:
         num = num**2
     mass_matrix = np.ones(2 * len(radii) * num)
     for j in np.arange(0, len(radii)):
-        mass_matrix[2*j*num:2*(j+1)*num] = (
-                radii[j]**2 * mass_matrix[2*j*num:2*(j+1)*num])
+        mass_matrix[2 * j * num : 2 * (j + 1) * num] = (
+            radii[j] ** 2 * mass_matrix[2 * j * num : 2 * (j + 1) * num]
+        )
     return mass_matrix
