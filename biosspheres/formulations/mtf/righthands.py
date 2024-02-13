3a363c126fd707399fdc0187af9c05234d7209ef
IMartinezA <33355502+IMartinezA@users.noreply.github.com>
2024-02-13 11:13:19 -0300

    Code reformatted with Black.

--- biosspheres/formulations/mtf/righthands.py
+++ biosspheres/formulations/mtf/righthands.py
@@ -4,10 +4,7 @@ import biosspheres.miscella.auxindexes as auxindexes
 
 
 def b_vector_1_sphere_mtf(
-        r: float,
-        pi_inv: float,
-        b_d: np.ndarray,
-        b_n: np.ndarray
+    r: float, pi_inv: float, b_d: np.ndarray, b_n: np.ndarray
 ) -> np.ndarray:
     """
 
@@ -29,19 +26,15 @@ def b_vector_1_sphere_mtf(
     """
     num = len(b_d)
     b = np.empty(4 * num, dtype=b_d.dtype)
-    b[0:num] = - r**2 * b_d
-    b[num:2*num] = - r**2 * b_n
-    b[2*num:3*num] = r**2 * b_d
-    b[3*num:4*num] = -pi_inv * r**2 * b_n
+    b[0:num] = -(r**2) * b_d
+    b[num : 2 * num] = -(r**2) * b_n
+    b[2 * num : 3 * num] = r**2 * b_d
+    b[3 * num : 4 * num] = -pi_inv * r**2 * b_n
     return b
 
 
 def b_vector_n_spheres_mtf_cte_function(
-        n: int,
-        big_l: int,
-        radii: np.ndarray,
-        cte: float,
-        azimuthal: bool = True
+    n: int, big_l: int, radii: np.ndarray, cte: float, azimuthal: bool = True
 ) -> np.ndarray:
     """
 
@@ -74,26 +67,28 @@ def b_vector_n_spheres_mtf_cte_function(
     b = np.zeros(4 * num * n)
     index = np.arange(0, n)
     b[2 * num * n + 2 * num * index] = (
-            cte * 2. * np.sqrt(np.pi) * radii[index]**2)
+        cte * 2.0 * np.sqrt(np.pi) * radii[index] ** 2
+    )
     b[2 * num * index] = -b[2 * num * n + 2 * num * index]
     return b
 
 
 def b_vector_n_spheres_mtf_point_source(
-        n: int,
-        big_l: int,
-        ps,
-        p0: np.ndarray,
-        rs: np.ndarray,
-        sigma_e: float,
-        x_dia: np.ndarray,
-        mass_n_two_j_blocks: np.ndarray
+    n: int,
+    big_l: int,
+    ps,
+    p0: np.ndarray,
+    rs: np.ndarray,
+    sigma_e: float,
+    x_dia: np.ndarray,
+    mass_n_two_j_blocks: np.ndarray,
 ) -> np.array:
     """
-    
+
     Notes
     -----
-    All arrays that represent vectors are in the cartesian coordinate system.
+    All arrays that represent vectors are in the cartesian coordinate
+    system.
 
     Parameters
     ----------
@@ -122,47 +117,61 @@ def b_vector_n_spheres_mtf_point_source(
     num = (big_l + 1) ** 2
     b = np.zeros(4 * num * n)
 
-    eles = np.arange(0, big_l+1)
+    eles = np.arange(0, big_l + 1)
     eles_plus_one = eles + 1
     eles_minus_one = eles - 1
     two_el_plus_one = 2 * eles + 1
     eles_times_eles_plus_one = eles * eles_plus_one
     l_times_l_plus_l_divided_by_2 = eles_times_eles_plus_one // 2
-    pesykus, p2_plus_p_plus_q, p2_plus_p_minus_q = \
-        auxindexes.pes_y_kus(big_l)
+    pesykus, p2_plus_p_plus_q, p2_plus_p_minus_q = auxindexes.pes_y_kus(big_l)
     for index in np.arange(0, n):
         vector = p0 - ps[index]
         p = np.linalg.norm(vector)
         phi = np.arctan2(vector[1], vector[0])
         ratio = rs[index] / p
-        legendre = \
-            pyshtools.legendre.PlmON(big_l, vector[2]/p, csphase=-1, cnorm=0)
+        legendre = pyshtools.legendre.PlmON(
+            big_l, vector[2] / p, csphase=-1, cnorm=0
+        )
         temp_p = ratio**eles / (p * two_el_plus_one * sigma_e)
-        temp_p_d = \
-            -eles * ratio ** eles_minus_one \
-            / (p**2 * two_el_plus_one * sigma_e)
-        b[2*num*index + eles_times_eles_plus_one] = \
+        temp_p_d = (
+            -eles * ratio**eles_minus_one / (p**2 * two_el_plus_one * sigma_e)
+        )
+        b[2 * num * index + eles_times_eles_plus_one] = (
             temp_p * legendre[l_times_l_plus_l_divided_by_2]
-        b[(2 * index + 1) * num + eles_times_eles_plus_one] = \
+        )
+        b[(2 * index + 1) * num + eles_times_eles_plus_one] = (
             temp_p_d * legendre[l_times_l_plus_l_divided_by_2]
+        )
 
-        b[2 * num * index + p2_plus_p_plus_q] = \
-            temp_p[pesykus[:, 0]] * \
-            legendre[l_times_l_plus_l_divided_by_2[pesykus[:, 0]]
-                     + pesykus[:, 1]] * np.cos(phi * pesykus[:, 1])
-        b[2 * num * index + p2_plus_p_minus_q] = \
-            temp_p[pesykus[:, 0]] * \
-            legendre[l_times_l_plus_l_divided_by_2[pesykus[:, 0]]
-                     + pesykus[:, 1]] * np.sin(phi * pesykus[:, 1])
+        b[2 * num * index + p2_plus_p_plus_q] = (
+            temp_p[pesykus[:, 0]]
+            * legendre[
+                l_times_l_plus_l_divided_by_2[pesykus[:, 0]] + pesykus[:, 1]
+            ]
+            * np.cos(phi * pesykus[:, 1])
+        )
+        b[2 * num * index + p2_plus_p_minus_q] = (
+            temp_p[pesykus[:, 0]]
+            * legendre[
+                l_times_l_plus_l_divided_by_2[pesykus[:, 0]] + pesykus[:, 1]
+            ]
+            * np.sin(phi * pesykus[:, 1])
+        )
 
-        b[(2 * index + 1) * num + p2_plus_p_plus_q] = \
-            temp_p_d[pesykus[:, 0]] * \
-            legendre[l_times_l_plus_l_divided_by_2[pesykus[:, 0]]
-                     + pesykus[:, 1]] * np.cos(phi * pesykus[:, 1])
-        b[(2 * index + 1) * num + p2_plus_p_minus_q] = \
-            temp_p_d[pesykus[:, 0]] * \
-            legendre[l_times_l_plus_l_divided_by_2[pesykus[:, 0]]
-                     + pesykus[:, 1]] * np.sin(phi * pesykus[:, 1])
-    b[2 * num * n: 4 * num * n] = b[0: 2 * num * n] * x_dia
-    b[0: 2 * num * n] = -b[0: 2 * num * n] * mass_n_two_j_blocks
+        b[(2 * index + 1) * num + p2_plus_p_plus_q] = (
+            temp_p_d[pesykus[:, 0]]
+            * legendre[
+                l_times_l_plus_l_divided_by_2[pesykus[:, 0]] + pesykus[:, 1]
+            ]
+            * np.cos(phi * pesykus[:, 1])
+        )
+        b[(2 * index + 1) * num + p2_plus_p_minus_q] = (
+            temp_p_d[pesykus[:, 0]]
+            * legendre[
+                l_times_l_plus_l_divided_by_2[pesykus[:, 0]] + pesykus[:, 1]
+            ]
+            * np.sin(phi * pesykus[:, 1])
+        )
+    b[2 * num * n : 4 * num * n] = b[0 : 2 * num * n] * x_dia
+    b[0 : 2 * num * n] = -b[0 : 2 * num * n] * mass_n_two_j_blocks
     return b
--- biosspheres/formulations/mtf/solvertemplates.py
+++ biosspheres/formulations/mtf/solvertemplates.py
@@ -9,12 +9,12 @@ import biosspheres.miscella.harmonicex as harmonicex
 
 
 def mtf_laplace_one_sphere_point_source_azimuthal_direct_solver(
-        big_l: int,
-        r: float,
-        sigma_e: float,
-        sigma_i: float,
-        distance: float,
-        intensity: float,
+    big_l: int,
+    r: float,
+    sigma_e: float,
+    sigma_i: float,
+    distance: float,
+    intensity: float,
 ) -> np.ndarray:
     """
     Returns the solution of the system
@@ -23,12 +23,12 @@ def mtf_laplace_one_sphere_point_source_azimuthal_direct_solver(
     right hand corresponding to an external excitation of the function
     biossphere.miscella.mathfunctions.point_source(
         r, [0, 0, distance], sigma_e).
-    
+
     Notes
     -----
     Write details about what it is being solved.
-    
-    
+
+
     Parameters
     ----------
     big_l : int
@@ -51,38 +51,36 @@ def mtf_laplace_one_sphere_point_source_azimuthal_direct_solver(
 
     """
     pi = sigma_i / sigma_e
-    
+
     # Build of phi_e.
-    b_d = (
-        harmonicex.
-        point_source_coefficients_dirichlet_expansion_azimuthal_symmetry(
-            big_l, r, distance, sigma_e, intensity))
-    b_n = (
-        harmonicex.
-        point_source_coefficients_neumann_expansion_0j_azimuthal_symmetry(
-            big_l, r, distance, sigma_e, intensity))
-    b = righthands.b_vector_1_sphere_mtf(r, 1. / pi, b_d, b_n)
-    
+    b_d = harmonicex.point_source_coefficients_dirichlet_expansion_azimuthal_symmetry(
+        big_l, r, distance, sigma_e, intensity
+    )
+    b_n = harmonicex.point_source_coefficients_neumann_expansion_0j_azimuthal_symmetry(
+        big_l, r, distance, sigma_e, intensity
+    )
+    b = righthands.b_vector_1_sphere_mtf(r, 1.0 / pi, b_d, b_n)
+
     # Build of mtf matrix
     a_0 = laplaceself.a_0j_matrix(big_l, r, azimuthal=True)
     a_1 = laplaceself.a_j_matrix(big_l, r, azimuthal=True)
     matrix = mtf.mtf_1_matrix(r, pi, a_0, a_1)
     solution = np.linalg.solve(matrix, b)
-    
+
     return solution
 
 
 def mtf_laplace_n_spheres_point_source_direct_solver(
-        n: int,
-        big_l: int,
-        big_l_c: int,
-        radii: np.ndarray,
-        center_positions: list[np.ndarray],
-        sigmas: np.ndarray,
-        p0: np.ndarray,
+    n: int,
+    big_l: int,
+    big_l_c: int,
+    radii: np.ndarray,
+    center_positions: list[np.ndarray],
+    sigmas: np.ndarray,
+    p0: np.ndarray,
 ) -> np.ndarray:
     """
-    
+
     Parameters
     ----------
     n : int
@@ -90,7 +88,8 @@ def mtf_laplace_n_spheres_point_source_direct_solver(
     big_l : int
         >= 0, max degree.
     big_l_c : int
-        >= 0. It's the parameter used to compute the points of the quadrature.
+        >= 0. It's the parameter used to compute the points of the
+        quadrature.
     radii : np.ndarray
         Array with the radii of the spheres.
     center_positions : list[np.ndarray]
@@ -107,40 +106,45 @@ def mtf_laplace_n_spheres_point_source_direct_solver(
 
     """
     # Build of the right hand side.
-    pii = sigmas[1:len(sigmas)] / sigmas[0]
+    pii = sigmas[1 : len(sigmas)] / sigmas[0]
     mass_n_two = mass.n_two_j_blocks(big_l, radii, azimuthal=False)
     x_dia, x_dia_inv = mtf.x_diagonal_with_its_inv(
-        n, big_l, radii, pii, azimuthal=False)
+        n, big_l, radii, pii, azimuthal=False
+    )
     b = righthands.b_vector_n_spheres_mtf_point_source(
-        n, big_l, center_positions, p0, radii, sigmas[0], x_dia, mass_n_two)
+        n, big_l, center_positions, p0, radii, sigmas[0], x_dia, mass_n_two
+    )
     # Build of mtf matrix
     big_a_0_cross = laplacecross.all_cross_interactions_n_spheres_v2d(
-        n, big_l, big_l_c, radii, center_positions)
+        n, big_l, big_l_c, radii, center_positions
+    )
     sparse_big_a_0_self, sparse_big_a_n = laplaceself.a_0_a_n_sparse_matrices(
-        n, big_l, radii, azimuthal=False)
+        n, big_l, radii, azimuthal=False
+    )
     matrix = mtf.mtf_n_matrix(
-        big_a_0_cross, sparse_big_a_0_self, sparse_big_a_n, x_dia, x_dia_inv)
-    
+        big_a_0_cross, sparse_big_a_0_self, sparse_big_a_n, x_dia, x_dia_inv
+    )
+
     del big_a_0_cross, sparse_big_a_0_self, sparse_big_a_n, mass_n_two, x_dia
     del x_dia_inv, pii
-    
+
     solution = np.linalg.solve(matrix, b)
-    
+
     return solution
 
 
 def mtf_laplace_n_spheres_point_source_indirect_solver(
-        n: int,
-        big_l: int,
-        big_l_c: int,
-        radii: np.ndarray,
-        center_positions: list[np.ndarray],
-        sigmas: np.ndarray,
-        p0: np.ndarray,
-        tolerance: float
+    n: int,
+    big_l: int,
+    big_l_c: int,
+    radii: np.ndarray,
+    center_positions: list[np.ndarray],
+    sigmas: np.ndarray,
+    p0: np.ndarray,
+    tolerance: float,
 ) -> np.ndarray:
     """
-    
+
     Parameters
     ----------
     n : int
@@ -148,7 +152,8 @@ def mtf_laplace_n_spheres_point_source_indirect_solver(
     big_l : int
         >= 0, max degree.
     big_l_c : int
-        >= 0. It's the parameter used to compute the points of the quadrature.
+        >= 0. It's the parameter used to compute the points of the
+        quadrature.
     radii : np.ndarray
         Array with the radii of the spheres.
     center_positions : list[np.ndarray]
@@ -164,30 +169,34 @@ def mtf_laplace_n_spheres_point_source_indirect_solver(
     -------
     solution : np.ndarray
         of floats. Length 4 * n * (big_l + 1)**2.
-        
+
     """
     # Build of the right hand side.
-    pii = sigmas[1:len(sigmas)] / sigmas[0]
+    pii = sigmas[1 : len(sigmas)] / sigmas[0]
     mass_n_two = mass.n_two_j_blocks(big_l, radii, azimuthal=False)
     x_dia, x_dia_inv = mtf.x_diagonal_with_its_inv(
-        n, big_l, radii, pii, azimuthal=False)
+        n, big_l, radii, pii, azimuthal=False
+    )
     b = righthands.b_vector_n_spheres_mtf_point_source(
-        n, big_l, center_positions, p0, radii, sigmas[0], x_dia, mass_n_two)
+        n, big_l, center_positions, p0, radii, sigmas[0], x_dia, mass_n_two
+    )
     # Build of mtf matrix
     big_a_0_cross = laplacecross.all_cross_interactions_n_spheres_v2d(
-        n, big_l, big_l_c, radii, center_positions)
+        n, big_l, big_l_c, radii, center_positions
+    )
     sparse_big_a_0_self, sparse_big_a_n = laplaceself.a_0_a_n_sparse_matrices(
-        n, big_l, radii, azimuthal=False)
-    
+        n, big_l, radii, azimuthal=False
+    )
+
     linear_operator = mtf.mtf_n_linear_operator_v1(
-        big_a_0_cross, sparse_big_a_0_self, sparse_big_a_n, x_dia, x_dia_inv)
-    
+        big_a_0_cross, sparse_big_a_0_self, sparse_big_a_n, x_dia, x_dia_inv
+    )
+
     del big_a_0_cross, sparse_big_a_0_self, sparse_big_a_n, mass_n_two, x_dia
     del x_dia_inv, pii
-    
+
     solution, info = scipy.sparse.linalg.gmres(
-        linear_operator, b,
-        tol=tolerance,
-        restart=(4 * (big_l + 1)**2)**3)
-    
+        linear_operator, b, tol=tolerance, restart=(4 * (big_l + 1) ** 2) ** 3
+    )
+
     return solution
