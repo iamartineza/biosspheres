039aca676047fc573c88fc93e34bd5a1174beab2
iamartineza <iamartinez@uc.cl>
2024-02-16 17:58:58 -0300

    Changes in docstrings, refactor of linear_function
    Docstrings added for the functions:
    - cte
    - linear_function
    - plane_wave
    Docstring modified:
    - point_source

--- biosspheres/miscella/mathfunctions.py
+++ biosspheres/miscella/mathfunctions.py
@@ -2,22 +2,80 @@ import numpy as np
 
 
 def cte(c: float) -> float:
+    """
+    Constant function.
+
+    Parameters
+    ----------
+    c: float
+        constant parameter.
+
+    Returns
+    -------
+    c: float
+        constant result.
+    """
     return c
 
 
-def linear_function_3d(coefficients: np.ndarray, vector: np.ndarray) -> float:
+def linear_function(coefficients: np.ndarray, vector: np.ndarray) -> float:
+    """
+    Linear function.
+
+    Notes
+    -----
+    All arrays that represent vectors are in the cartesian coordinate
+    system.
+
+    Parameters
+    ----------
+    coefficients: np.ndarray
+        of floats, parameters of the linear function.
+    vector: np.ndarray
+        of floats, variable.
+
+    Returns
+    -------
+    float
+        result of the linear function given the parameters and the
+        vector variable.
+    """
     return np.dot(coefficients, vector)
 
 
 def plane_wave(
     a: float, k: np.ndarray, r: np.ndarray, p: np.ndarray
 ) -> complex:
+    """
+    Plane wave function, input in three dimensions.
+
+    Notes
+    -----
+    All arrays that represent vectors are in the cartesian coordinate
+    system.
+
+    Parameters
+    ----------
+    a: float
+        amplitude.
+    k: np.ndarray
+        wave vector.
+    r: np.ndarray
+        variable.
+    p: np.ndarray
+
+    Returns
+    -------
+    complex
+        result of the plane wave function given the parameters and the
+        vector variable.
+    """
     return a * np.exp(np.dot(k, (r + p)) * 1j)
 
 
 def point_source(r: np.ndarray, p: np.ndarray, sigma_e: float) -> float:
     """
-    Returns 1. / (4 * pi * sigma_e * ||r - p||_2)
+    Returns 1. / (4 * pi * sigma_e * ||r - p||_2), where
 
     Notes
     -----
