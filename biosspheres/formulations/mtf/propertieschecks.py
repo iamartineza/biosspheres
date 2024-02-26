db7d606318fec8fcb1f931342635ee4c5291cc9f
iamartineza <iamartinez@uc.cl>
2024-02-26 17:46:04 -0300

    Refactor of variable.

--- biosspheres/formulations/mtf/propertieschecks.py
+++ biosspheres/formulations/mtf/propertieschecks.py
@@ -9,27 +9,27 @@ def calderon_and_jump_checks_n_spheres(
     x_dia: np.ndarray,
     mass_n_two: np.ndarray,
     b: np.ndarray,
-    solution2: np.ndarray,
+    solution: np.ndarray,
 ):
     # Discrete Calderon errors:
     aux_index = len(big_a_0_cross[0])
     exterior_calderon = np.linalg.norm(
         2.0
         * (
-            np.matmul(big_a_0_cross, solution2[0:aux_index])
-            + sparse_big_a_0_self.dot(solution2[0:aux_index])
+            np.matmul(big_a_0_cross, solution[0:aux_index])
+            + sparse_big_a_0_self.dot(solution[0:aux_index])
         )
-        - mass_n_two * solution2[0:aux_index]
+        - mass_n_two * solution[0:aux_index]
     )
     interior_calderon = np.linalg.norm(
-        2.0 * sparse_big_a_n.dot(solution2[aux_index : 2 * aux_index])
-        - mass_n_two * solution2[aux_index : 2 * aux_index]
+        2.0 * sparse_big_a_n.dot(solution[aux_index: 2 * aux_index])
+        - mass_n_two * solution[aux_index: 2 * aux_index]
     )
 
     # Jump error
     jump_error = np.linalg.norm(
-        -solution2[0:aux_index] * x_dia
-        + mass_n_two * solution2[aux_index : 2 * aux_index]
+        -solution[0:aux_index] * x_dia
+        + mass_n_two * solution[aux_index: 2 * aux_index]
         - b[aux_index : 2 * aux_index]
     )
     return exterior_calderon, interior_calderon, jump_error
