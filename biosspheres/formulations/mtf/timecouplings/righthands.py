15c458a04df5d225833889db5c9b4ce3be9c26f9
iamartineza <iamartinez@uc.cl>
2024-09-26 07:28:40 -0300

    Added a righthand side with a point source

--- biosspheres/formulations/mtf/timecouplings/righthands.py
+++ biosspheres/formulations/mtf/timecouplings/righthands.py
@@ -1,4 +1,6 @@
 import numpy as np
+import biosspheres.formulations.massmatrices as mass
+import biosspheres.formulations.mtf.mtf as mtf
 import biosspheres.formulations.mtf.righthands as righthands
 
 
@@ -17,6 +19,37 @@ def phi_part_of_b_cte_space_and_time(
 ):
     b = righthands.b_vector_n_spheres_mtf_cte_function(
         n, big_l, radii, cte, azimuthal=False
+    )
+
+    def b_phi_part(time: float) -> np.ndarray:
+        return b
+
+    return b_phi_part
+
+
+def phi_part_of_b_point_source_space_and_cte_time(
+    big_l: int,
+    n: int,
+    radii: np.ndarray,
+    center_positions,
+    p0,
+    sigma_e,
+    pii: np.ndarray,
+    amplitude: float,
+):
+    x_dia, x_dia_inv = mtf.x_diagonal_with_its_inv(
+        n, big_l, radii, pii, azimuthal=False
+    )
+    mass_n_two_j_blocks = mass.n_two_j_blocks(big_l, radii, azimuthal=False)
+    b = amplitude * righthands.b_vector_n_spheres_mtf_point_source(
+        n,
+        big_l,
+        center_positions,
+        p0,
+        radii,
+        sigma_e,
+        x_dia,
+        mass_n_two_j_blocks,
     )
 
     def b_phi_part(time: float) -> np.ndarray:
