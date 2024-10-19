4070e84609c241794dfd9b48d6036de95dbf4adb
iamartineza <iamartinez@uc.cl>
2024-10-19 19:27:04 -0300

    Addition of linear function and some changes
    Also:
    - Input validation for b_vector_1_sphere_mtf
    - Use of auxindedexes.eles_combination in function b_vector_n_spheres_mtf_point_source

--- biosspheres/formulations/mtf/righthands.py
+++ biosspheres/formulations/mtf/righthands.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pyshtools
 import biosspheres.miscella.harmonicex as harmonicex
+import biosspheres.quadratures.sphere as quadratures
 import biosspheres.utils.auxindexes as auxindexes
+import biosspheres.utils.validation.inputs as valin
 
 
 def b_vector_1_sphere_mtf(
@@ -11,7 +13,7 @@ def b_vector_1_sphere_mtf(
 
     Parameters
     ----------
-    r: float
+    r : float
         > 0, radius of the sphere
     pi_inv : float
         > 0, adimensional parameter.
@@ -25,6 +27,15 @@ def b_vector_1_sphere_mtf(
     b : numpy array
         Length 4 * length(b_d)
     """
+    valin.radius_validation(r, "r")
+    valin.pii_validation(pi_inv, "pi_inv")
+    valin.numpy_array_validation(b_d, "b_d")
+    valin.numpy_array_validation(b_n, "b_n")
+    valin.dimensions_array_validation(b_d, "b_d", 1)
+    valin.dimensions_array_validation(b_n, "b_n", 1)
+    valin.finite_values_in_array(b_d, "b_d")
+    valin.finite_values_in_array(b_n, "b_n")
+
     num = len(b_d)
     b = np.empty(4 * num, dtype=b_d.dtype)
     b[0:num] = -(r**2) * b_d
@@ -74,6 +85,61 @@ def b_vector_n_spheres_mtf_cte_function(
     return b
 
 
+def b_vector_n_spheres_mtf_linear_function_z(
+    big_l: int, n: int, ps, cte: float, radii: np.ndarray, x_dia: np.ndarray
+) -> np.array:
+    """
+    Function = cte * z.
+
+    Notes
+    -----
+    All arrays that represent vectors are in the cartesian coordinate
+    system.
+
+    Parameters
+    ----------
+    big_l : int
+        >= 1, max degree.
+    n : int
+        >= 1, number of spheres.
+    ps : list of numpy arrays
+        the numpy arrays are of length 3, and represent
+        the position vectors of the center of the spheres.
+    cte : float
+         Constant parameter.
+    radii : np.ndarray
+        of the radii of the spheres.
+    x_dia : np.ndarray
+        diagonal of the matrix X.
+
+    Returns
+    -------
+    b: np.ndarray
+        of 1 dimension and length 4 * n * (big_l+1)**2
+    """
+    num = (big_l + 1) ** 2
+    b = np.zeros(4 * num * n)
+
+    mini_el = 1
+    num_mini_el = (mini_el + 1) ** 2
+    indexes = np.arange(0, num_mini_el)
+
+    final_length, pre_vector, transform = (
+        quadratures.real_spherical_harmonic_transform_1d(mini_el, mini_el)
+    )
+    for index in np.arange(0, n):
+        b[2 * num * index + indexes] = np.sum(
+            (radii[index] * pre_vector[2, :] + ps[index][2]) * cte * transform,
+            axis=1,
+        )
+        b[(2 * index + 1) * num + indexes] = np.sum(
+            (-pre_vector[2, :] * cte) * transform, axis=1
+        )
+    b[2 * num * n : 4 * num * n] = b[0 : 2 * num * n] * x_dia
+    b[0 : 2 * num * n] = -b[0 : 2 * num * n]
+    return b
+
+
 def b_vector_n_spheres_mtf_point_source(
     n: int,
     big_l: int,
@@ -118,12 +184,12 @@ def b_vector_n_spheres_mtf_point_source(
     num = (big_l + 1) ** 2
     b = np.zeros(4 * num * n)
 
-    eles = np.arange(0, big_l + 1)
-    eles_plus_one = eles + 1
+    eles, eles_times_eles_plus_one, l_times_l_plus_l_divided_by_2 = (
+        auxindexes.eles_combination(big_l)
+    )
     eles_minus_one = eles - 1
     two_el_plus_one = 2 * eles + 1
-    eles_times_eles_plus_one = eles * eles_plus_one
-    l_times_l_plus_l_divided_by_2 = eles_times_eles_plus_one // 2
+
     pesykus, p2_plus_p_plus_q, p2_plus_p_minus_q = auxindexes.pes_y_kus(big_l)
     for index in np.arange(0, n):
         vector = p0 - ps[index]
